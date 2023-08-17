216823058724ab49f1f119413c0d5db54af8256b
SomethingSomethingOrOther <abdoulgdia@gmail.com>
2023-08-17 12:59:29 -0400

    added calculate-investment function and fixed inputs not to show NaN

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+
 
 function App() {
+  const [currentSavings, setCurrentSavings] = useState(10000);
+  const [yearlySavings, setYearlySavings] = useState(500);
+  const [expectedInterest, setExpectedInterest] = useState(8);
+  const [investmentDuration, setInvestmentDuration] = useState(10);
+  const [investmentData, setInvestmentData] = useState([]);
+     
+
+   const calculateInvestment=()=>{
+         const data=[]
+         let totalSavings=currentSavings
+         for (let year=1;year<=investmentDuration;year++){
+            totalSavings += yearlySavings
+             const interestEarned = (totalSavings * expectedInterest) / 100
+            totalSavings += interestEarned
+            data.push({year,totalSavings,interestEarned})
+         }
+         setInvestmentData(data)
+   }
+
+
+   const handleReset=()=>{
+    setInvestmentData([])
+   }
+
+
+   const handleCurrentSavings=(e)=>{
+        const value=e.target.value
+      if (!isNaN(value)){
+        setCurrentSavings(value)
+      }
+   }
+
+
+   const handleYearlySavings=(e)=>{
+    const value=e.target.value
+  if (!isNaN(value)){
+    setYearlySavings(value)
+  }
+}
+
+
+const handleExpectedInterest=(e)=>{
+  const value=e.target.value
+if (!isNaN(value)){
+  setExpectedInterest(value)
+}
+}
+const handleInvestmentDuration=(e)=>{
+  const value=e.target.value
+if (!isNaN(value)){
+  setInvestmentDuration(value)
+}
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>
+          Investment Calculator
+        </h1>
+      </div>
+      <div>
+        <label>
+          Current Savings($):
+          <input onChange={handleCurrentSavings}
+          value={currentSavings}
+          type="text" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Yearly Savings($):
+          <input onChange={handleYearlySavings} 
+          value={yearlySavings} 
+          type="text" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Expected Interest(%,per Year):
+          <input onChange={handleExpectedInterest} 
+          value={expectedInterest} 
+          type="text" />
+        </label>
+      </div>
+      <div>
+        <label>
+          Investment Duration (Years):
+          <input 
+          onChange={handleInvestmentDuration} 
+          value={investmentDuration} 
+          type="text" />
+        </label>
+      </div>
+      <div>
+        <button onClick={calculateInvestment}>Calculate</button>
+        <button onClick={handleReset}>Rest</button>
+      </div>
+      <div>
+        {
+            investmentData.length > 0 && 
+            <div>
+              <h3>Investment Data</h3>
+              <ul>
+                {investmentData.map((item)=>(
+                  <>
+                  <li>Year <b>{item.year}</b>: {item.totalSavings.toFixed(2)}</li>{" "}
+                 <span>Interest Earned:{item.interestEarned.toFixed(2)}</span>
+                  </>
+                ))}
+              </ul>
+            </div>
+        }        
+      </div>
     </div>
   );
 }
